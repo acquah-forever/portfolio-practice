3d2acf85bb7cd2bb08027f77cc09b042e81ba633
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-20 22:20:25 +0000

    updated Portfolio.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,56 +20,53 @@ const Projects = () => {
   }
 
   return (
-    <div className='px-7  min-h-screen flex flex-col sm:grid sm:grid-cols-2 justify-items-center'>
-      <motion.div variants={parent} initial="hidden" whileInView="visible" viewport={{ amount: 0.4, once: true }} className='mt-10 flex flex-col rounded-3xl max-w-xs sm:max-w-sm h-110 sm:h-130 border'>
-        <motion.div variants={children} className='mt-8 flex justify-center'>
-          <img className='object-cover px-12 sm:px-5 max-w-xs sm:max-w-sm h-auto mb-7 hover:scale-130 sm:hover:scale-105 transition-all ease-in-out duration-300' src={Landing} alt="" />
+    <div className='px-6 py-16  min-h-screen grid gap-10 sm:grid sm:grid-cols-2 mx-auto max-w-6xl'>
+      <motion.div variants={parent} initial="hidden" whileInView="visible" viewport={{ amount: 0.4, once: true }} className='w-full flex flex-col rounded-3xl border border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-900 shadow-md hover:shadow-xl  transition-all duration-300 overflow-hidden'>
+        <motion.div variants={children} className='p-6 flex flex-col flex-grow"'>
+          <img className='w-full h-60 sm:h-72 md:h-80 object-cover transition-transform duration-500 hover:scale-105' src={Landing} alt="" />
         </motion.div>
 
-        <motion.div variants="children" className='px-6'>
-          <h1 className='text-center sm:text-left text-lg sm:text-xl mb-3'>MY LANDING PAGE</h1>
-          <div className='mb-5'>
-            <h2 className='mb-3 text-slate-500 text-center sm:text-left dark:text-slate-300'>DESCRIPTION</h2>
-            <motion.h2 className='text-center text-sm sm:text-md sm:text-left' variants={children}>A landing page exhibiting my skills
-              <br />
-              in React JS and TailwindCSS.</motion.h2>
+        <motion.div variants={children} className='px-6'>
+          <h1 className='text-xl font-semibold mb-4 text-center sm:text-left'>MY LANDING PAGE</h1>
+          <div className='mb-6'>
+            <h2 className='text-slate-500 dark:text-slate-400 mb-2 text-sm tracking-wide text-center sm:text-left'>DESCRIPTION</h2>
+            <motion.p className='text-sm text-slate-700 dark:text-slate-300 text-center sm:text-left' variants={children}>A landing page exhibiting my skills in React JS and TailwindCSS..</motion.p>
           </div>
-          <motion.div variants={children} className='flex flex-col space-y-5'>
-            <h1 className='text-slate-500 text-center sm:text-left dark:text-slate-300'>BUILT WITH</h1>
+          <motion.div variants={children} className='mt-auto'>
+            <h1 className='text-slate-500 dark:text-slate-400 mb-3 text-sm tracking-wide text-center sm:text-left'>BUILT WITH</h1>
 
-            <motion.div variants={children} className='flex justify-center sm:justify-start space-x-4 will-change-transform'>
-              <img className='w-10 h-10 md:w-12 md:h-12 object-cover' src={JS} alt="icon" />
-              <img className='w-10 h-10 md:w-12 md:h-12 object-cover' src={REACT} alt="icon" />
-              <img className='w-10 h-10 md:w-12 md:h-12 object-cover' src={TAILWIND} alt="icon" />
+            <div variants={children} className='flex justify-center sm:justify-start gap-4'>
+              <img className='w-10 h-10 md:w-12 md:h-12' src={JS} alt="icon" />
+              <img className='w-10 h-10 md:w-12 md:h-12' src={REACT} alt="icon" />
+              <img className='w-10 h-10 md:w-12 md:h-12' src={TAILWIND} alt="icon" />
 
-            </motion.div>
+            </div>
           </motion.div>
         </motion.div>
       </motion.div >
 
 
-      <motion.div variants={parent} initial="hidden" whileInView="visible" viewport={{ amount: 0.4, once: true }} className='mt-10 flex flex-col rounded-3xl max-w-xs sm:max-w-sm h-115 sm:h-130 border'>
-        <motion.div variants={children} className='mt-8 flex justify-center'>
-          <img className='object-cover px-12 sm:px-5 max-w-xs sm:max-w-sm h-auto mb-7 hover:scale-130 sm:hover:scale-105 transition-all ease-in-out duration-300' src={Landing1} alt="" />
+      <motion.div variants={parent} initial="hidden" whileInView="visible" viewport={{ amount: 0.4, once: true }} className='w-full flex flex-col rounded-3xl border border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-900 shadow-md hover:shadow-xl  transition-all duration-300 overflow-hidden'>
+        <motion.div variants={children} className='p-6 flex flex-col flex-grow"'>
+          <img className='w-full h-60 sm:h-72 md:h-80 object-cover transition-transform duration-500 hover:scale-105' src={Landing1} alt="" />
         </motion.div>
 
-        <motion.div variants="children" className='px-6'>
-          <h1 className='text-lg sm:text-xl text-center sm:text-left mb-3'>AI AUTOMATION</h1>
-          <div className='mb-5'>
-            <h2 className='text-slate-500 mb-3 text-center sm:text-left dark:text-slate-300'>DESCRIPTION</h2>
-            <motion.h2 className='text-center sm:text-left text-sm sm:text-md' variants={children}>A page exhibiting my skills
-              <br />
-              in React JS and TailwindCSS.</motion.h2>
+        <motion.div variants={children} className='px-6'>
+          <h1 className='text-xl font-semibold mb-4 text-center sm:text-left'>AI AUTOMATION</h1>
+          <div className='mb-6'>
+            <h2 className='text-slate-500 dark:text-slate-400 mb-2 text-sm tracking-wide text-center sm:text-left'>DESCRIPTION</h2>
+            <motion.p className='text-center sm:text-left text-sm sm:text-md' variants={children}>A page exhibiting my skills in React JS and TailwindCSS.</motion.p>
           </div>
-          <motion.div variants={children} className='flex flex-col space-y-5'>
-            <h1 className='text-slate-500 text-center sm:text-left dark:text-slate-300'>BUILT WITH</h1>
+          <motion.div variants={children} className='mt-auto'>
+            <h1 className='text-slate-500 dark:text-slate-400 mb-3 text-sm tracking-wide text-center sm:text-left'>BUILT WITH</h1>
 
-            <motion.div variants={children} className='flex justify-center sm:justify-start space-x-4 will-change-transform'>
-              <img className='w-10 h-10 md:w-12 md:h-12 object-cover' src={JS} alt="icon" />
-              <img className='w-10 h-10 md:w-12 md:h-12 object-cover' src={REACT} alt="icon" />
-              <img className='w-10 h-10 md:w-12 md:h-12 object-cover' src={TAILWIND} alt="icon" />
+            <div variants={children} className='flex justify-center sm:justify-start gap-4'>
+              <img className='w-10 h-10 md:w-12 md:h-12' src={JS} alt="icon" />
+              <img className='w-10 h-10 md:w-12 md:h-12' src={REACT} alt="icon" />
+              <img className='w-10 h-10 md:w-12 md:h-12' src={TAILWIND} alt="icon" />
 
-            </motion.div>
+            </div>
+
           </motion.div>
         </motion.div>
       </motion.div >
