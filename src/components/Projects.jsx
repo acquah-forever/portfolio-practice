8f96edaa60a9082b2c46f8a7d608fd78898ea31b
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-21 17:32:06 +0000

    updated portfolio props

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Projects = () => {
+const Projects = ({photo,title,description,js,tail,react}) => {
+
   return (
     <div>
-      
+      <img src={photo} alt="image" />
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+      <h1>Built With:</h1>
+      <img src={js} alt="javascript" />
+      <img src={tail} alt="tailwind" />
+      <img src={react} alt="react" />
     </div>
   )
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import Projects from '../components/Projects'
+import Landing from '../assets/landing page.jpeg'
+import Landing1 from '../assets/landing page1.jpeg'
+import react from '../assets/react.png'
+import tailwind from '../assets/tailwind.png'
+import javascript from '../assets/js.png'
 
 
 const Portfolio = () => {
+
+  let data = [
+    {
+      id: 1,
+      photo: Landing,
+      description: 'A page exhibiting my skills in React JS, Tailwind and JavaScript',
+      js: javascript,
+      tail: tailwind,
+      react: react
+    },
+    {
+      id: 2,
+      photo: Landing1,
+      description: 'A page exhibiting my skills in Recat JS, Tailwind and Javascript',
+      js: javascript,
+      tail: tailwind,
+      react: react
+    }
+  ]
+
+
   return (
     <div>
-      
+      {data.map((e) =>(<Projects 
+      key={e.id}
+      photo={e.photo}
+      description={e.description}
+      js={e.js}
+      tail={e.tail}
+      react={e.react}
+      />))}
     </div>
   )
 }
