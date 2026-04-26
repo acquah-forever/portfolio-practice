c9939f1fce8df40d0980d8fdbddbea4e3516c584
acquah-forever <kojoaddo598@gmail.com>
2026-04-26 23:13:17 +0000

    changed the background image of my-Ecommerce

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -49,7 +49,7 @@ const Projects = () => {
                     <div className="card bg-base-100 w-full shadow-sm">
                         <figure>
                             <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                src="https://images.unsplash.com/photo-1664455340023-214c33a9d0bd?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                                 alt="Shoes" />
                         </figure>
                         <div className="card-body">
