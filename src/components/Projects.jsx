728daab8c8fd83ecbc725dff7ad3b082fd0a4c66
acquah-forever <kojoaddo598@gmail.com>
2026-04-26 23:10:46 +0000

    changed the background image of my-RealEstate

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -25,7 +25,7 @@ const Projects = () => {
                     <div className="card bg-base-100 w-full shadow-sm">
                         <figure>
                             <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                                 alt="Shoes" />
                         </figure>
                         <div className="card-body">
